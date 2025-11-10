95997411e9165f9b6cac181f5ff91b7f5b46634f
tianfu-1 <tianfuxu66@gmail.com>
2025-11-10 23:14:20 +0800

    将游戏logo链接和游戏的index链接都替换成assets的二级域名

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "kpopgamesgo.com",
+        hostname: "assets.kpopgamesgo.com",
       },
     ],
   },
